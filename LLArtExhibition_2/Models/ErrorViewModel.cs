4d3f2df00e8e32701e8f6da57cb686fbf0dd7a9f
71md <87401180+71md@users.noreply.github.com>
2023-09-02 13:31:33 +0800

    111

--- LLArtExhibition_2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LLArtExhibition_2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
